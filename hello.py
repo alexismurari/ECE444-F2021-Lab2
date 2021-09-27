ff16e379bd8789a2d8c6fee540d8c68ad9ca2a1c
Alexis Murari <alexis.murari@mail.utoronto.ca>
2021-09-26 22:09:52 -0400

    Added utoronto email check to the page

--- hello.py
+++ hello.py
@@ -1,33 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = StringField('What is your UofT Email address?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+def uoft(email):
+    if 'utoronto' in email:
+        return True
+    else:
+        return False
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    email = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        form.name.data = ''
-        form.email.data = ''
-    return render_template('index.html', form=form, name=name, email=email)
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.name.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'), email = session.get('email'), good = uoft(session.get('email')))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
