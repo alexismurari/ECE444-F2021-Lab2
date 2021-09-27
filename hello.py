8b8766b2fc23d79bc4689d620be88cdf478a1503
Alexis Murari <alexis.murari@mail.utoronto.ca>
2021-09-26 21:32:30 -0400

    First commit for activity 4. The email field is already added.

--- hello.py
+++ hello.py
@@ -2,16 +2,32 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    name = 'Alexis'
-    return render_template('user.html', name = name, current_time = datetime.utcnow())
-
+    name = None
+    email = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        form.name.data = ''
+        form.email.data = ''
+    return render_template('index.html', form=form, name=name, email=email)
 
 if __name__ == '__main__':
     app.run(debug=True)
